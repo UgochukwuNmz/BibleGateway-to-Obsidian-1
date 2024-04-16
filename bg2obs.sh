cf7dea42d1b23308be31afec2c990d4775eaf27d
Emmanuel Ihim <emmanuelihim7@gmail.com>
2024-04-16 03:33:24 -0500

    Added Bible tag to every Bible chapter so that I can use custom css in Obsidian

--- bg2obs.sh
+++ bg2obs.sh
@@ -35,13 +35,9 @@ biblefolder="${biblename} (${translation})"
 bookarray=(Genesis Exodus Leviticus Numbers Deuteronomy Joshua Judges Ruth "1 Samuel" "2 Samuel" "1 Kings" "2 Kings" "1 Chronicles" "2 Chronicles" Ezra Nehemiah Esther Job Psalms Proverbs Ecclesiastes "Song of Solomon" Isaiah Jeremiah Lamentations Ezekiel Daniel Hosea Joel Amos Obadiah Jonah Micah Nahum Habakkuk Zephaniah Haggai Zechariah Malachi Matthew Mark Luke John Acts Romans "1 Corinthians" "2 Corinthians" Galatians Ephesians Philippians Colossians "1 Thessalonians" "2 Thessalonians" "1 Timothy" "2 Timothy" Titus Philemon Hebrews James "1 Peter" "2 Peter" "1 John" "2 John" "3 John" Jude Revelation)
 lengtharray=(50 40 27 36 34 24 21 4 31 24 22 25 29 36 10 13 10 42 150 31 12 8 66 52 5 48 12 14 3 9 1 4 7 3 3 3 2 14 4 28 16 24 21 28 16 16 13 6 6 4 4 5 3 6 4 3 1 13 5 5 3 5 1 1 1 22)
 
-# # Initialise the main index file
-# echo -e "# $bible_folder" > "$bible_name.md"
-
 # Initialize the main index file
 index_file="./${biblefolder}/The Bible.md"
 echo "# The Bible" > "$index_file"
-
 
 for book_index in "${!bookarray[@]}"; do
     book="${bookarray[$book_index]}"
@@ -58,6 +54,8 @@ for book_index in "${!bookarray[@]}"; do
 
     for ((chapter=1; chapter <= maxchapter; chapter++)); do
         filename="${book} ${chapter}.md"
+        file_path="$folder_name/$filename"
+        temp_file_path="$folder_name/temp_$filename"
         ruby_command="ruby bg2md.rb -v \"$translation\" \"$book $chapter\" > \"$folder_name/$filename\""
 
         if [ "$verbose" = true ]; then
@@ -69,6 +67,9 @@ for book_index in "${!bookarray[@]}"; do
 
         # Add this chapter to the main index file
         echo -n " [[$filename|$chapter]]" >> "$index_file"
+
+        # Write the YAML front matter to the temporary file first
+        echo -e "---\ntags:\n  - Bible\n---" > "$temp_file_path"
 
         # Initialize breadcrumb links with checks for first and last chapters
         if [ $chapter -eq 1 ]; then
@@ -85,12 +86,16 @@ for book_index in "${!bookarray[@]}"; do
         book_link="[[$book|$book]]"
 
         # Format breadcrumbs with dividers correctly positioned
-        breadcrumbs_top="$prev_link $([ -n "$prev_link" ] && echo "| ")$book_link$([ -n "$next_link" ] && echo " | ")$next_link\n\n---\n\n"
-        breadcrumbs_bottom="\n\n---\n\n$prev_link $([ -n "$prev_link" ] && echo "| ")$book_link$([ -n "$next_link" ] && echo " | ")$next_link"
+        breadcrumbs_top="\n$prev_link $([ -n "$prev_link" ] && echo "| ")$book_link$([ -n "$next_link" ] && echo " | ")$next_link\n\n---\n"
+        breadcrumbs_bottom="\n---\n\n$prev_link $([ -n "$prev_link" ] && echo "| ")$book_link$([ -n "$next_link" ] && echo " | ")$next_link"
         
         # Prepend and append breadcrumbs to the top and bottom of the file
-        echo -e "$breadcrumbs_top$(cat "$folder_name/$filename")" > "$folder_name/$filename"
-        echo -e "$breadcrumbs_bottom" >> "$folder_name/$filename"
+        echo -e "$breadcrumbs_top" >> "$temp_file_path"
+        cat "$file_path" >> "$temp_file_path"
+        echo -e "$breadcrumbs_bottom" >> "$temp_file_path"
+
+        # Replace the original file with the modified temporary file
+        mv "$temp_file_path" "$file_path"
     done
 
     echo -e "\n" >> "$index_file"  # New line for the next book
